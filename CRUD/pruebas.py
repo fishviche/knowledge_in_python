b830f467024f989783115f82adc9e06ed566c79c
Fishviche <evalerianom@gmail.com>
2022-09-25 23:56:48 +0000

    Adding gunicorn to our project

--- CRUD/pruebas.py
+++
@@ -1,6 +0,0 @@
-with open("./sql/1_table.sql", "r") as f:
-    sentences = [x.replace("\n", "") + ";" for x in f.read().rsplit(";") if x != ""]
-    print(sentences)
-
-# print(sql_file)
-# print(sql_file.read().split(';'))
